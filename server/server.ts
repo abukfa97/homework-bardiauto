7c97e573dbef4d2f2fcb9abd103590844562dc0d
Adrián Bükfa <bukfa.adrian@gmail.com>
2023-07-07 22:02:10 +0200

    Create userService with getAllSeats & saveNewSeat

--- server/server.ts
+++ server/server.ts
@@ -10,4 +10,4 @@ app.use(express.json());
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
-})
+});
---
+++ server/services/userService.ts
@@ -0,0 +1,28 @@
+import { PrismaClient, Prisma, Seat } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+
+async function getAllSeats() : Promise<Seat[]> {
+    try{
+        const seats : Seat[] = await prisma.seat.findMany();
+        return seats;
+    } catch (error) {
+        console.error(error);
+        throw new Error('Failed to fetch seats from db');
+    }
+}
+
+interface seatInputDTO{
+    name: string
+}
+
+async function saveNewSeat(data: seatInputDTO) {
+    try{
+        const newSeat = prisma.seat.create({data});
+        return newSeat;
+    } catch (error) {
+        console.error(error);
+        console.log(`Failed to create element in db`);
+    }
+}
