b3d1ccc051262eb07528ae3a8fb7390538f0e447
Adrián Bükfa <bukfa.adrian@gmail.com>
2023-07-07 21:16:23 +0200

    Create server.ts

---
+++ server/server.ts
@@ -0,0 +1,13 @@
+import express, {Request, Response} from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+})
