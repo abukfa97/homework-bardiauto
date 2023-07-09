4ea5b63953023c192581ac30b34e8db730fe25df
Adrián Bükfa <bukfa.adrian@gmail.com>
2023-07-09 14:23:00 +0200

    Refactor reservationService

--- server/services/reservationService.ts
+++ server/services/reservationService.ts
@@ -1,23 +1,39 @@
 import nodemailer from 'nodemailer';
 import mailDetails from '../model/mailDetailsInterface';
 
+export default class ReservationService {
+    private static instance : ReservationService;
+    private transporter!: nodemailer.Transporter;
 
-
-const transporter  = nodemailer.createTransport({
-    service: "gmail",
-    host: "smtp.gmail.com",
-    port: 587,
-    auth: {
-        user: process.env.MAIL,
-        pass: process.env.MAIL_PASS,
+    constructor() {}
+    getInstance(){
+        if(!ReservationService.instance){
+            ReservationService.instance = new ReservationService();
+        }
+        return ReservationService.instance;
     }
-});
 
-const sendMail = async (mailDetails: mailDetails) => {
-    try {
-        const info = await transporter.sendMail(mailDetails);
-        console.log(`Email sent successfully!\nMESSAGE ID: ${info.messageId}`);
-    } catch (error) {
-        console.log(error)
-    }   
+    private createConnection(){
+        this.transporter = nodemailer.createTransport({
+            service: process.env.SMTP_SERVICE,
+            host: process.env.SMTP_HOST,
+            port: Number(process.env.SMTP_PORT),
+            auth: {
+                user: process.env.MAIL,
+                pass: process.env.MAIL_PASS,
+            }
+        });
+    }
+
+    async sendMail(mailDetails: mailDetails){
+        this.createConnection();
+        try {
+            const info = await this.transporter.sendMail(mailDetails);
+            console.log(`Email sent successfully!\nMESSAGE ID: ${info.messageId}`);
+        } catch (error) {
+            console.log(error);
+        }
+        return this.transporter.close();
+    }
+    
 }
