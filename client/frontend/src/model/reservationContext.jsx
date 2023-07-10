e498a213f1a5d40d5fbc4f2b3cf3b59b9cb3956c
Adrián Bükfa <bukfa.adrian@gmail.com>
2023-07-10 04:25:35 +0200

    Implement reservationContext:
    - addSeat function
    - removeSeat function
    - updateMail function

--- client/frontend/src/model/reservationContext.jsx
+++ client/frontend/src/model/reservationContext.jsx
@@ -6,8 +6,7 @@ const ReservationProvider =  ({children}) => {
     const [reservation, setReservation] = useState({
         seatCounter: 0,
         seats: [],
-        mail: "",
-        date: new Date()
+        mail: ""
     });
 
     const increaseSeatCount = () => {
@@ -23,4 +22,35 @@ const ReservationProvider =  ({children}) => {
             seatCounter: prevReservation.seatCounter - 1
         }));
     };
+
+    const addSeat = (seatName) => {
+        setReservation((prevReservation) => ({
+            ...prevReservation,
+            seats: [...prevReservation.seats, seatName]
+        }));
+    };
+
+    const removeSeat = (seatNameToRemove) => {
+        setReservation((prevReservation) => ({
+            ...prevReservation,
+            seats: prevReservation.seats.filter((seatName) => seatName !== seatNameToRemove)
+        }));
+    };
+
+    const updateMail = (newMail) => {
+        setReservation((prevReservation) => ({
+            ...prevReservation,
+            mail: newMail
+        }));
+    };
+
+    return (
+        <ReservationContext.Provider value={{reservation, increaseSeatCount, decreaseSeatCount, addSeat, removeSeat, updateMail}}>
+            {children}
+        </ReservationContext.Provider>
+    )
+
+    
 }
+
+export {ReservationContext, ReservationProvider}
