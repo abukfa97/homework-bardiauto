6539971527a37fac6fc7372587b0855dac452b25
Adrián Bükfa <bukfa.adrian@gmail.com>
2023-07-10 04:28:43 +0200

    Implement seatServices

---
+++ client/frontend/services/seatServices.jsx
@@ -0,0 +1,42 @@
+import axios from 'axios'
+
+export default class seatService{
+    
+
+    async registerSeatInProgress(seatName){
+        try{
+            await axios.post('/api/seats/',{name: seatName});
+        }catch(error){
+            console.error(error);
+        }
+    }
+
+    async isExistingSeat(seatName){
+        try{
+            let response = await axios.get(`/api/seats/isExists/${seatName}`);
+
+            console.log(response.data);
+            return response.data;
+        }catch(error){
+            console.error(error);
+        }
+    }
+
+    async deleteReservation(seatName){
+        try{
+            await axios.delete('/api/seats/', {name: seatName});
+        }catch(error){
+            console.error(error);
+        }
+    }
+
+    async sendReservation(reservation){
+        try{
+            let response = await axios.patch('/api/seats/finish-reservation', {names: reservation.seats, mail: reservation.mail});
+            return response.data;
+        }catch(error){
+            console.error(error);
+        }
+    }
+
+}
